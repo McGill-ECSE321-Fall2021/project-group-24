85c8724bdffdccc31fd101f72d41bccdbb65345d
Kevin Nam <dong.g.nam@mail.mcgill.ca>
2021-10-13 19:30:43 -0400

    making getters and setters

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Archive.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.librarysystem.model;
+
+import javax.persistence.Entity;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.Id;
+
+@Entity
+public class Archive{
+    
+}
