06bcca3d9ca5fba949a972c6343bc2c7c603e86c
Kevin Nam <dong.g.nam@mail.mcgill.ca>
2021-10-14 15:41:54 -0400

    Working on Persistence Testing (Not finished yet)
    Co-Authored-By: Nima Hemati <77599957+nimaaah@users.noreply.github.com>
    Co-Authored-By: Arman S-M <arman.shroff-mehrabadi@mail.mcgill.ca>

---
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/LibrarySystemRepository.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.librarysystem.dao;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+//import ca.mcgill.ecse321.eventregistration.model.Person;
+//import ca.mcgill.ecse321.eventregistration.model.Event;
+
+@Repository
+public class LibrarySystemRepository {
+
+  @Autowired
+  EntityManager entityManager;
+  //GOT TO 2.2.2
+
+}
