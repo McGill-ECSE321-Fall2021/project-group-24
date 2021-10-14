51dad4e908868f1ba20d2d4807e4c8050beb6f20
Nafis Rahman <nafisrahman18@gmail.com>
2021-10-14 16:44:28 -0400

    did some thangs
    created Patron methods in LibrarySystemRepository
    
    Co-Authored-By: skarya22 <51128536+skarya22@users.noreply.github.com>
    Co-Authored-By: selenajin99 <68762346+selenajin99@users.noreply.github.com>

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibrarySystemRepository.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibrarySystemRepository.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import ca.mcgill.ecse321.librarysystem.model.*;
+
 //import ca.mcgill.ecse321.eventregistration.model.Person;
 //import ca.mcgill.ecse321.eventregistration.model.Event;
 
@@ -19,4 +21,50 @@ public class LibrarySystemRepository {
   EntityManager entityManager;
   //GOT TO 2.2.2
 
+
+	@Transactional
+	public Patron createPatronIRL(String idNum, String firstName, String lastName, boolean isResident, String address, String email) {
+		Patron p = new Patron();
+		p.setFirstName(firstName);
+		p.setLastName(lastName);
+		p.setIdNum(idNum);
+		p.setIsResident(isResident);
+		p.setIsRegisteredOnline(false);
+		p.setEmail(email);
+		p.setAddress(address);
+		p.setIsVerified(true);
+		p.setPassword(null);
+		p.setUsername(null);
+		
+		entityManager.persist(p);
+		return p;
+	}
+	
+	
+	public Patron createPatronOnline(String idNum, String firstName, String lastName, boolean isResident, String address, String email, String username, String password) {
+		Patron p = new Patron();
+		p.setUsername(username);
+		p.setPassword(password);
+		p.setFirstName(firstName);
+		p.setLastName(lastName);
+		p.setEmail(email);
+		p.setIdNum(idNum);
+		p.setIsResident(isResident);
+		p.setAddress(address);
+		p.setIsVerified(false);
+		p.setIsRegisteredOnline(true);
+
+		entityManager.persist(p);
+		return p;		
+	}
+	
+	
+	public Patron getPatron(String idNum) {
+		Patron p = entityManager.find(Patron.class, idNum);
+		return p;
+	}
+	
+	
+	
+  
 }
