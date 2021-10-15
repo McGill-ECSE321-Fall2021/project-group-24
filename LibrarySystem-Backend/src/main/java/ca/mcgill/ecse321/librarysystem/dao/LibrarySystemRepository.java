32b5b8dc9e31abc34a4cab8f4a1e2ddf0914c52c
selenajin99 <yangyang.jin@mail.mcgill.ca>
2021-10-14 20:20:36 -0400

    add persistence methods

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibrarySystemRepository.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibrarySystemRepository.java
@@ -20,8 +20,9 @@ public class LibrarySystemRepository {
   @Autowired
   EntityManager entityManager;
   //GOT TO 2.2.2
+private String itemNumber;
 
-
+  	// by Nafis, Saggar, Selena
 	@Transactional
 	public Patron createPatronIRL(String idNum, String firstName, String lastName, boolean isResident, String address, String email) {
 		Patron p = new Patron();
@@ -63,6 +64,113 @@ public class LibrarySystemRepository {
 		Patron p = entityManager.find(Patron.class, idNum);
 		return p;
 	}
+	
+	
+	// by Selena
+	public Book createBook(String itemTitle, String description, String imageURL,String publisher, String author, boolean isCheckedOut, String genre, Date publishDate, boolean isReservable) {
+		Book b = new Book();
+		b.setItemTitle(itemTitle);
+		b.setDescription(description);
+		b.setAuthor(author);
+		b.setGenre(genre);
+		b.setIsCheckedOut(isCheckedOut);
+		b.isIsReservable();
+		b.setPublishDate(publishDate);
+		b.setItemNumber(itemNumber);
+		b.setPublisher(publisher);
+		b.setIsReservable(isReservable); // should this be true?
+		entityManager.persist(b);
+		return b;
+	}
+	
+	public Book getBook(String itemNumber) {
+		Book book = entityManager.find(Book.class, itemNumber);
+		return book;
+	}
+
+	public MusicAlbum createMusicAlbum(String description, String genre, String imageUrl, String itemTitle, Date publishDate, boolean isCheckedOut, String recordingLabel, String artist) {
+		MusicAlbum m = new MusicAlbum();
+		m.setDescription(description);
+		m.setGenre(genre);
+		m.setImageUrl(imageUrl);
+		m.setIsCheckedOut(isCheckedOut);
+		m.setIsReservable(true);
+		m.setItemNumber(itemNumber);
+		m.setItemTitle(itemTitle);
+		m.setPublishDate(publishDate);
+		m.setRecordingLabel(recordingLabel);
+		m.setArtist(artist);
+		entityManager.persist(m);
+		return m;
+	}
+	
+	public MusicAlbum getMusicAlbum(String itemNumber) {
+		MusicAlbum musicAlbum = entityManager.find(MusicAlbum.class, itemNumber);
+		return musicAlbum;
+	}
+
+	public Movie createMovie(String description, String genre, String imageUrl, String itemTitle, Date publishDate, String productionCompany, String cast, String director, boolean isCheckedOut) {
+		Movie m = new Movie();
+		m.setDescription(description);
+		m.setGenre(genre);
+		m.setImageUrl(imageUrl);
+		m.setIsCheckedOut(isCheckedOut);
+		m.setIsReservable(true);
+		m.setItemNumber(itemNumber);
+		m.setItemTitle(itemTitle);
+		m.setPublishDate(publishDate);
+		m.setProductionCompany(productionCompany);
+		m.setCast(cast);
+		m.setDirector(director);
+		entityManager.persist(m);
+		return m;
+	}
+	
+	public Movie getMovie(String itemNumber) {
+		Movie movie = entityManager.find(Movie.class, itemNumber);
+		return movie;
+	}
+
+	public PrintedMedia createPrintedMedia(String description, String genre, String imageUrl, String itemTitle, Date publishDate, String issueNumber, boolean isCheckedOut) {
+		PrintedMedia pm = new PrintedMedia();
+		pm.setDescription(description);
+		pm.setGenre(genre);
+		pm.setImageUrl(imageUrl);
+		pm.setIsCheckedOut(isCheckedOut);
+		pm.setIsReservable(true);
+		pm.setItemNumber(itemNumber);
+		pm.setItemTitle(itemTitle);
+		pm.setPublishDate(publishDate);
+		pm.setIssueNumber(issueNumber);
+		entityManager.persist(pm);
+		return pm;
+	}
+	
+	public PrintedMedia getPrintedMedia(String itemNumber) {
+		PrintedMedia item = entityManager.find(PrintedMedia.class, itemNumber);
+		return item;
+	}
+
+	public Archive createArchive(String description, String genre, String imageUrl, String itemTitle, Date publishDate) {
+		Archive a = new Archive();
+		a.setDescription(description);
+		a.setGenre(genre);
+		a.setImageUrl(imageUrl);
+		a.setIsCheckedOut(false);
+		a.setIsReservable(false);
+		a.setItemNumber(itemNumber);
+		a.setItemTitle(itemTitle);
+		a.setPublishDate(publishDate);
+		entityManager.persist(a);
+		return a;
+		
+	}
+	public Archive getArchive(String itemNumber) {
+		Archive archive = entityManager.find(Archive.class, itemNumber);
+		return archive;
+	}
+	
+	
 	
 	
 	
