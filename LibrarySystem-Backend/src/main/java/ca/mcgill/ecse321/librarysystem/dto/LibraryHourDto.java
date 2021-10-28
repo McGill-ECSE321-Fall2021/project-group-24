d3976c7779b79ec7e2c3a835e4a4721bd314f209
Arman Shroff-Mehrabadi <arman.shroff-mehrabadi@mail.mcgill.ca>
2021-10-28 15:57:29 -0400

    LibraryHourDto and ShiftDto done

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/LibraryHourDto.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/LibraryHourDto.java
@@ -1,5 +1,37 @@
 package ca.mcgill.ecse321.librarysystem.dto;
 
-public class LibraryHourDto {
+import java.sql.Time;
 
+import ca.mcgill.ecse321.librarysystem.model.LibraryHour.DayOfWeek;
+
+public class LibraryHourDto {
+	DayOfWeek dayOfWeek; 
+	Time startTime; 
+	Time endTime; 
+	
+	public LibraryHourDto() {
+	}
+	
+	// default opening hours are set to 9am-5pm
+	public LibraryHourDto(DayOfWeek dayOfWeek) {
+		this(dayOfWeek, Time.valueOf("09:00:00"), Time.valueOf("17:00:00")); 
+	}
+	
+	public LibraryHourDto(DayOfWeek dayOfWeek, Time startTime, Time endTime) {
+		this.dayOfWeek = dayOfWeek;
+		this.startTime = startTime; 
+		this.endTime = endTime; 
+	}
+	
+	public DayOfWeek getDayOfWeek() {
+		return this.dayOfWeek; 
+	}
+	
+	public Time getStartTime() {
+		return this.startTime;
+	}
+	
+	public Time getEndTime() {
+		return this.endTime; 
+	}
 }
