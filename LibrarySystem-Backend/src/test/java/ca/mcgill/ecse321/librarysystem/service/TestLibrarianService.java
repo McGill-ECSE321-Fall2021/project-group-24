b775d46ea9a04a80e8bbc1987d82ce982275327a
Nima Hemati <nima.hemati@mail.mcgill.ca>
2021-11-09 00:00:23 -0500

    Pushing
    pushing with Kevin and Selena
    
    Co-Authored-By: Kevin <48372730+namdgk@users.noreply.github.com>
    Co-Authored-By: selenajin99 <68762346+selenajin99@users.noreply.github.com>

---
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/TestLibrarianService.java
@@ -0,0 +1,113 @@
+package ca.mcgill.ecse321.librarysystem.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.librarysystem.dao.*;
+import ca.mcgill.ecse321.librarysystem.model.*;
+import ca.mcgill.ecse321.librarysystem.service.UserService;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TestLibrarianService {
+
+    @Mock
+    private LibrarianRepository librarianDao;
+
+    @InjectMocks
+    // private UserService userService; 
+	private LibrarianService librarianService; 
+
+	private static final String emptyString = "";
+    // private static final String testIdNum = "bobLibrarian-0";
+    // private static final String wrongIdNum = "jeffLibrarian-0";
+    private static final String testFirstName = "bobFirstName";
+    private static final String wrongFirstName = "jeffFirstName";
+    private static final String testLastName = "bobLastName";
+	private static final String wrongLastName = "jeffLastName";
+	private static final String testAddress = "bob st";
+	private static final String wrongAddress = "jeff st";
+	private static final String testEmail = "bob@bob.ca";
+	private static final String wrongEmail = "jeff@jeff.ca";
+	private static final String testUsername = "bobUsername";
+	private static final String wrongUsername = "jeffUsername";
+	private static final String testPassword = "bobPassword";
+	private static final String wrongPassword = "bobPassword";
+
+
+
+    
+    @BeforeEach
+    public void setMockOutput()
+    {
+        lenient().when(librarianDao.findUserByUsername(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(testUsername))
+            {
+                Librarian librarian = new Librarian();
+                librarian.setFirstName(testFirstName);
+                librarian.setLastName(testLastName);
+                librarian.setAddress(testAddress);
+                librarian.setEmail(testEmail);
+                librarian.setUsername(testUsername);
+				librarian.setPassword(testPassword);
+                return librarian;
+            } else
+            {
+                return null;
+            }
+        });
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+            return invocation.getArgument(0);
+        };
+        lenient().when(librarianDao.save(any(Librarian.class))).thenAnswer(returnParameterAsAnswer);
+	}
+    @Test
+    public void testGetAllLibrarians()
+    {
+        ArrayList<Librarian> librarians = null;
+        try
+        {
+            librarians = new ArrayList<Librarian>(librarianService.getAllLibrarians());
+
+        } catch (IllegalArgumentException e)
+        {
+            fail();
+        }
+        assertNotNull(librarians);
+        assertEquals(1, librarians.size());
+		assertEquals(testFirstName, librarians.get(0).getFirstName());
+        assertEquals(testLastName, librarians.get(0).getLastName());
+        assertEquals(testAddress, librarians.get(0).getAddress());
+        assertEquals(testEmail, librarians.get(0).getEmail());
+        assertEquals(testUsername, librarians.get(0).getUsername());
+        assertEquals(testPassword, librarians.get(0).getPassword());
+    }
+
+}
+
+
