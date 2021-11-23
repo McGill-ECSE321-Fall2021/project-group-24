aeeeb24566bce4d865914f5efa8a06b243a0ebe0
selenajin99 <yangyang.jin@mail.mcgill.ca>
2021-11-23 11:46:30 -0500

    Update Login.vue

--- LibrarySystem-Frontend/src/components/Login.vue
+++ LibrarySystem-Frontend/src/components/Login.vue
@@ -1,24 +1,12 @@
 <template>
   <!--Make sure you only have one element in your template, or you will have an error. Wrap everything with a div -->
-  <div>
-    <a-button
-      type="primary"
-      @click="
-        createBook(
-          'title',
-          'deccy',
-          'url',
-          'pubby',
-          'authy',
-          'genre',
-          '2001-09-22',
-          true,
-          'admin'
-        )
-      "
-    >
-      Sign Up
-    </a-button>
+  <div id="loginInputs">
+    <div class="inputs">
+      <a-input placeholder="Username"> </a-input>
+    </div>
+    <div class="inputs">
+      <a-input placeholder="Password"> </a-input>
+    </div>
     <a-button
       type="primary"
       @click="
@@ -39,6 +27,22 @@
     </a-button>
   </div>
 </template>
-<script src="./Items.js"></script>
 
-<style></style>
+<style>
+.inputs {
+  margin: 10px auto;
+  position: relative;
+  align-self: center;
+  width: 20%;
+}
+
+#loginInputs {
+  /* padding-left: 30%;
+  padding-right: 30%; */
+  position: relative;
+  width: 100%;
+  margin: auto;
+  align-content: center;
+  align-self: center;
+}
+</style>
