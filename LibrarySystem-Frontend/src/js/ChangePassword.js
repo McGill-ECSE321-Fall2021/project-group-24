33eacf3f65396b33380c242d020c822f6f92e200
selenajin99 <yangyang.jin@mail.mcgill.ca>
2021-11-28 12:46:23 -0500

    fixed pop up for change user info
    need to add reload the page to display changes

--- LibrarySystem-Frontend/src/js/ChangePassword.js
+++ LibrarySystem-Frontend/src/js/ChangePassword.js
@@ -11,128 +11,116 @@ var AXIOS = axios.create({
   headers: { "Access-Control-Allow-Origin": frontendUrl },
 });
 
+export default {
+  name: "User",
+  data() {
+    return {
+      users: [],
+      newUser: "",
+      userError: "",
+      response: "",
+      responseStatus: null,
+      results: [],
+      visible: false,
+      currentUser: this.$store.state.currentUser,
+      formLayout: "horizontal",
+      form: this.$form.createForm(this, { name: "coordinated" }),
+      user: null,
+    };
+  },
 
-export default{
-    name: "User",
-    data(){
-        return{
-            users: [],
-            newUser: "",
-            userError: "",
-            response: [],
-            responseStatus: null,
-            results: [],
-            visible: false,
-            currentUser: this.$store.state.currentUser,
-            formLayout: "horizontal",
-            form: this.$form.createForm(this, { name: "coordinated" }),
-            user: null,
-        };
+  methods: {
+    showModal: function () {
+      this.visible = true;
+      this.userError = "";
     },
 
+    handleOk: function (e) {
+      console.log(e);
+      this.visible = false;
+    },
 
-    methods: {
-        showModal: function () {
-            this.visible = true;
-            this.userError = "";
-        },
-      
-        handleOk: function (e) {
-            console.log(e);
-            this.visible = false;
-        },
+    goBack() {
+      this.$router.push("/edit");
+    },
 
-        goBack(){
-            this.$router.push('/edit');
-        },
+    handleConfirmBlur(e) {
+      const value = e.target.value;
+      this.confirmDirty = this.confirmDirty || !!value;
+    },
 
+    compareToFirstPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && value !== form.getFieldValue("newPassword")) {
+        callback("The two passwords do not match!");
+      } else {
+        callback();
+      }
+    },
 
-        handleConfirmBlur(e) {
-            const value = e.target.value;
-            this.confirmDirty = this.confirmDirty || !!value;
-        },
-      
-        compareToFirstPassword(rule, value, callback) {
-            const form = this.form;
-            if (value && value !== form.getFieldValue("newPassword")) {
-              callback("The two passwords do not match!");
-            }
-            else{    
-              callback();
-            }
-        },
-      
-        validateToNextPassword(rule, value, callback) {
-            const form = this.form;
-            if (value && this.confirmDirty) {
-              form.validateFields(["confirm"], { force: true });
-            }
-            callback();
-        },
+    validateToNextPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && this.confirmDirty) {
+        form.validateFields(["confirm"], { force: true });
+      }
+      callback();
+    },
 
+    changePassword: function (username, newPassword, currentPassword) {
+      console.log(username);
+      AXIOS.put(
+        "/api/user/change_password/?username=" +
+          this.currentUser.username +
+          "&newPass=" +
+          newPassword +
+          "&oldPass=" +
+          currentPassword
+      )
+        .then((response) => {
+          this.users.filter((user) => {
+            return user.username;
+          });
+        })
+        .catch((e) => {
+          this.visible = true;
+          var errorMsg = e.response.data.error;
+          this.userError = errorMsg;
+        });
+    },
 
-        changePassword: function(username, newPassword, currentPassword){
-            console.log(username);
-            AXIOS.put(
-                "/api/user/change_password/?username="+
-                this.currentUser.username+
-                "&newPass="+
-                newPassword+
-                "&oldPass="+
-                currentPassword
-            ).then((response) => {
-                this.users.filter((user)=>{
-                    return user.username;
-                });
-            }).catch((e) => {
-                this.visible=true;
-                var errorMsg = e.response.data.error;
-                this.userError = errorMsg;
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Recieved values of form: ", values);
+          this.user = values;
+          console.log("any object", this.user.email);
+          AXIOS.put(
+            "/api/user/change_password/?username=" +
+              this.currentUser.username +
+              "&newPass=" +
+              this.user.newPassword +
+              "&oldPass=" +
+              this.user.password
+          )
+            .then((res) => {
+              this.visible = true;
+              this.responseStatus = res.status;
+              if (this.responseStatus === 200) {
+                this.$store.commit("changeUser", res.data);
+                this.response = "Password changed";
+                //this.$router.push("EditAccountDetails");
+              }
+
+              return res.status;
+            })
+            .catch((e) => {
+              this.visible = true;
+              var errorMsg = e.response.data;
+              this.userError = errorMsg;
             });
-        },
-
-        handleSubmit(e) {
-            e.preventDefault();
-            this.form.validateFields((err, values) => {
-              if (!err) {
-                    console.log("Recieved values of form: ", values);
-                    this.user = values;
-                    console.log("any object", this.user.email);
-                    AXIOS.put(
-                        "/api/user/change_password/?username="+
-                        this.currentUser.username+
-                        "&newPass="+
-                        this.user.newPassword+
-                        "&oldPass="+
-                        this.user.password
-                    )
-                    .then((res) => {
-                        console.log("RESPONSE: "+res.status);
-                        this.visible = true;
-                        this.responseStatus = res.status;
-                        console.log(res.data);
-                        this.$store.commit("changeUser", res.data);
-                        if (this.responseStatus == 200) {
-                            // window.location.href = "http://127.0.0.1:8087/#/";
-                            this.$router.push('/');
-                        }
-                          return res.status;
-                    })
-                    .catch((e) => {
-                          this.visible = true;
-                          var errorMsg = e.response.data;
-                          this.userError = errorMsg;
-                    });
-                }
-                  
-              
-            });
-        },
-        
-
-      
-        
-    }
-
-
-}
+        }
+      });
+    },
+  },
+};
--- LibrarySystem-Frontend/src/js/EditAccountDetails.js
+++ LibrarySystem-Frontend/src/js/EditAccountDetails.js
@@ -10,131 +10,128 @@ var AXIOS = axios.create({
   headers: { "Access-Control-Allow-Origin": frontendUrl },
 });
 
+export default {
+  name: "User",
+  data() {
+    return {
+      users: [],
+      newUser: "",
+      userError: "",
+      response: [],
+      responseStatus: null,
+      results: [],
+      visible: false,
+      currentUser: this.$store.state.currentUser,
+      formLayout: "horizontal",
+      form: this.$form.createForm(this, { name: "coordinated" }),
+      user: null,
+      response: "",
+    };
+  },
 
-export default{
-    name: "User",
-    data(){
-        return {
-            users: [],
-            newUser: "",
-            userError: "",
-            response: [],
-            responseStatus: null,
-            results: [],
-            visible: false,
-            currentUser: this.$store.state.currentUser,
-            formLayout: "horizontal",
-            form: this.$form.createForm(this, { name: "coordinated" }),
-            user: null,
-          };
+  methods: {
+    showModal: function () {
+      this.visible = true;
+      this.error = "";
     },
 
-    methods: {
-        showModal: function () {
-            this.visible = true;
-            this.error = "";
-        },
+    handleOk: function (e) {
+      console.log(e);
+      this.visible = false;
+    },
 
-        handleOk: function (e) {
-            console.log(e);
-            this.visible = false;
-        },
+    goHome() {
+      this.$router.push("/");
+    },
 
-        goHome(){
-            this.$router.push('/');
-        },
+    goToChangePassword() {
+      this.$router.push("/changePassword");
+    },
 
-        goToChangePassword(){
-            this.$router.push('/changePassword');
-        },
-        
-        goToDeleteAccount(){
-            this.$router.push('/deleteAccount');
-        },
+    goToDeleteAccount() {
+      this.$router.push("/deleteAccount");
+    },
 
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+          this.user = values;
 
-        handleSubmit(e){
-            e.preventDefault();
-            this.form.validateFields((err, values)=>{
-                if(!err){
-                    console.log("Received values of form: ", values);
-                    this.user = values;
-
-                    AXIOS.put(
-                        "/api/user/change_name/?username="+
-                        this.currentUser.username+
-                        "&firstName="+
-                        this.user.firstname+
-                        "&lastName="+
-                        this.user.lastname
-
-                    ).then((res)=>{
-
-                        console.log("RESPONSE: " + res.status);
-                        this.visible = true;
-                        this.responseStatus = res.status;
-                        console.log(res.data);
-                        console.log("THEN: ");
-                        if (this.responseStatus == 200 || this.responseStatus===201) {
-                            AXIOS.put(
-                                "/api/user/change_email/?username="+
-                                this.currentUser.username+
-                                "&email="+
-                                this.user.emailinput
-
-                            ).then((res)=>{
-                                this.visible = true;
-                                this.responseStatus = res.status;
-                                console.log(res.data);
-                                console.log("THEN: ");
-                                if (this.responseStatus == 200 || this.responseStatus===201) {
-                                    AXIOS.put(
-                                        "/api/user/change_address/?username="+
-                                        this.currentUser.username+
-                                        "&address="+
-                                        this.user.addressinput
-                                    ).then((res)=>{
-                                        this.visible = true;
-                                        this.responseStatus = res.status;
-                                        console.log(res.data);
-                                        this.$store.commit("changeUser", res.data);
-                                        if (this.responseStatus == 200 || this.responseStatus===201) {
-                                            this.$router.pus('/');
-                                        }
-                                        return res.status;
-                                    }).catch((e)=>{
-                                        console.log("CATCH: ");
-                                        this.visible = true;
-                                        console.log("line 109");
-                                        this.userError = e.response.data;
-                                    });
-
-                                }
-                            }).catch((e)=>{
-                                console.log("CATCH: ");
-                                this.visible = true;
-                                var errorMsg = e.response.data;
-                                console.log("line 118");
-                                this.userError = errorMsg;
-                            });
-                        }
-                    }).catch((e) => {
-                        console.log("CATCH: ");
-                        this.visible = true;
-                        var errorMsg = e.response.data;
-                        console.log("line 126");
-                        this.userError = errorMsg;
-                    });
-                }
-            }).catch((e)=>{
-                console.log("CATCH: ");
-                this.visible = true;
-                var errorMsg = e.response.data;
-                console.log("line 134");
-                this.userError = errorMsg;
+          AXIOS.put(
+            "/api/user/change_name/?username=" +
+              this.currentUser.username +
+              "&firstName=" +
+              this.user.firstname +
+              "&lastName=" +
+              this.user.lastname
+          )
+            .then((res) => {
+              this.visible = true;
+              this.responseStatus = res.status;
+              if (this.responseStatus == 200 || this.responseStatus == 201) {
+                AXIOS.put(
+                  "/api/user/change_email/?username=" +
+                    this.currentUser.username +
+                    "&email=" +
+                    this.user.emailinput
+                )
+                  .then((res) => {
+                    this.visible = true;
+                    this.responseStatus = res.status;
+                    console.log(res.data);
+                    console.log("THEN: ");
+                    if (
+                      this.responseStatus == 200 ||
+                      this.responseStatus === 201
+                    ) {
+                      AXIOS.put(
+                        "/api/user/change_address/?username=" +
+                          this.currentUser.username +
+                          "&address=" +
+                          this.user.addressinput
+                      )
+                        .then((res) => {
+                          this.visible = true;
+                          this.responseStatus = res.status;
+                          this.$store.commit("changeUser", res.data);
+                          if (
+                            this.responseStatus == 200 ||
+                            this.responseStatus === 201
+                          ) {
+                            this.response = "User Information saved";
+                            //this.$router.push("EditAccountDetails");
+                          }
+                          return res.status;
+                        })
+                        .catch((e) => {
+                          console.log("CATCH: ");
+                          this.visible = true;
+                          console.log("line 109");
+                          this.userError = e.response.data;
+                        });
+                    }
+                  })
+                  .catch((e) => {
+                    console.log("CATCH: ");
+                    this.visible = true;
+                    var errorMsg = e.response.data;
+                    console.log("line 118");
+                    this.userError = errorMsg;
+                  });
+              }
+            })
+            .catch((e) => {
+              console.log("CATCH: ");
+              this.visible = true;
+              var errorMsg = e.response.data;
+              console.log(e.response.data);
+              console.log("line 126");
+              this.userError = errorMsg;
             });
         }
-    }
-
-
-}
+      });
+    },
+  },
+};
