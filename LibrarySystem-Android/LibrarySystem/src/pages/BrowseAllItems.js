4247fa314fa5ecbfe5ac5184cc568af2aa6ae35f
Saagar <saagarkarya@gmail.com>
2021-12-02 16:28:55 -0500

    searchbar

--- LibrarySystem-Android/LibrarySystem/src/pages/BrowseAllItems.js
+++ LibrarySystem-Android/LibrarySystem/src/pages/BrowseAllItems.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button, Searchbar} from 'react-native-paper';
 
 import axios from 'axios';
 import ItemCard from '../components/ItemCard';
@@ -24,6 +24,45 @@ const BrowseAllItems = ({navigation}) => {
       });
   };
 
+  const search = query => {
+    setResults(
+      items.filter(item => {
+        if (
+          item.itemTitle.toLowerCase().includes(query.toLowerCase()) ||
+          item.itemNumber.toLowerCase().includes(query.toLowerCase()) ||
+          item.description.toLowerCase().includes(query.toLowerCase()) ||
+          (item.author &&
+            item.author.toLowerCase().includes(query.toLowerCase())) ||
+          (item.genre &&
+            item.genre.toLowerCase().includes(query.toLowerCase())) ||
+          (item.publisher &&
+            item.publisher.toLowerCase().includes(query.toLowerCase())) ||
+          (item.issueNumber &&
+            item.issueNumber.toLowerCase().includes(query.toLowerCase())) ||
+          (item.movieCase &&
+            item.movieCase.toLowerCase().includes(query.toLowerCase())) ||
+          (item.productionCompany &&
+            item.productionCompany
+              .toLowerCase()
+              .includes(query.toLowerCase())) ||
+          (item.director &&
+            item.director.toLowerCase().includes(query.toLowerCase())) ||
+          (item.producer &&
+            item.producer.toLowerCase().includes(query.toLowerCase())) ||
+          (item.artist &&
+            item.artist.toLowerCase().includes(query.toLowerCase())) ||
+          (item.recordingLabel &&
+            item.recordingLabel.toLowerCase().includes(query.toLowerCase())) ||
+          (item.type && item.type.toLowerCase().includes(query.toLowerCase()))
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
+    );
+  };
+
   //use 'state variables' for when you want data to change. ie for when u pull from database
   const [loading, setLoading] = useState(true);
   //the items array is initialized to an empty array '[]', and 'setItems' is a function for when
@@ -37,41 +76,44 @@ const BrowseAllItems = ({navigation}) => {
   }, []);
 
   return (
-    //a flatlist takes in an array as 'data', and a function 'renderItem' tells it what to render for each
-    //element in the array.
-    <FlatList
-      data={items}
-      //refreshing and onRefresh let the user swipe down to refresh the page.
-      refreshing={loading}
-      onRefresh={() => {
-        setLoading(true);
-        getItems();
-      }}
-      renderItem={({item}) => {
-        return (
-          //ItemCard is a component I made in /src/components, so that
-          //any time we want to edit the way an item is shown, it will
-          //do so on every page that we want an ItemCard on.
-          <ItemCard
-            item={item}
-            //I am passing a custom parameter "buttons" so that we can have different buttons
-            //each time that the ItemCard is used.
-            //On this page, we want the reserve button, but on the ReserveItem page,
-            //we do not need any buttons.
-            buttons={
-              <Button
-                //this is how to pass an object to the next page. Take a look at
-                //ReserveRoom.js to see how the item is used there.
-                onPress={() => {
-                  navigation.navigate('ReserveItem', {item});
-                }}>
-                Reserve
-              </Button>
-            }
-          />
-        );
-      }}
-    />
+    <>
+      <Searchbar onChangeText={search} />
+      {/* a flatlist takes in an array as 'data', and a function 'renderItem'
+      tells it what to render for each //element in the array. */}
+      <FlatList
+        data={results}
+        //refreshing and onRefresh let the user swipe down to refresh the page.
+        refreshing={loading}
+        onRefresh={() => {
+          setLoading(true);
+          getItems();
+        }}
+        renderItem={({item}) => {
+          return (
+            //ItemCard is a component I made in /src/components, so that
+            //any time we want to edit the way an item is shown, it will
+            //do so on every page that we want an ItemCard on.
+            <ItemCard
+              item={item}
+              //I am passing a custom parameter "buttons" so that we can have different buttons
+              //each time that the ItemCard is used.
+              //On this page, we want the reserve button, but on the ReserveItem page,
+              //we do not need any buttons.
+              buttons={
+                <Button
+                  //this is how to pass an object to the next page. Take a look at
+                  //ReserveRoom.js to see how the item is used there.
+                  onPress={() => {
+                    navigation.navigate('ReserveItem', {item});
+                  }}>
+                  Reserve
+                </Button>
+              }
+            />
+          );
+        }}
+      />
+    </>
   );
 };
 
