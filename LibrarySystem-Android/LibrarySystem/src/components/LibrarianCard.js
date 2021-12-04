3c9e74794172c6cc43226976e4710ac51a892841
Arman Shroff-Mehrabadi <arman.shroff-mehrabadi@mail.mcgill.ca>
2021-12-04 17:52:45 -0500

    partway done
    #100 #103

---
+++ LibrarySystem-Android/LibrarySystem/src/components/LibrarianCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+import {Button, Card, Title, Paragraph, Text} from 'react-native-paper';
+
+// Author: Arman
+// The card for each librarian has their name, address, & email, as well as a button to view their shifts
+const LibrarianCard = ({librarian, Dbutton, Vbutton}) => {
+  return (
+    <Card
+      style={{
+        alignSelf: 'center',
+        width: '90%',
+        borderRadius: 15,
+        elevation: 2,
+        marginVertical: 10,
+      }}>
+      <Card.Content style={{alignItems: 'center'}}>
+        <Title>{librarian.firstName}</Title>
+        <Paragraph>First Name: {librarian.firstName}</Paragraph>
+        <Paragraph>Last Name: {librarian.lastName}</Paragraph>
+        <Paragraph>Address: {librarian.address}</Paragraph>
+        <Paragraph>Email: {librarian.email}</Paragraph>
+      </Card.Content>
+      <Card.Actions style={{alignSelf: 'center'}}>{someShiftsButton}</Card.Actions>
+    </Card>
+  );
+};
+
+export default LibrarianCard;
--- LibrarySystem-Android/LibrarySystem/src/pages/ViewAllLibrarians.js
+++ LibrarySystem-Android/LibrarySystem/src/pages/ViewAllLibrarians.js
@@ -1,19 +1,111 @@
+// @author: Arman
 import React from 'react';
 import {View, Text} from 'react-native';
 import axios from 'axios';
 import {ScrollView} from 'react-native-gesture-handler';
 const baseUrl = 'https://librarysystem-backend-321.herokuapp.com/';
-//this is from vue js file
+import LibrarianCard from '../components/LibrarianCard';
+
+//this is from the vue js file
 var AXIOS = axios.create({
   baseURL: baseUrl,
 });
 
 const ViewAllLibrarians = () => {
+  const [loading, setLoading] = useState(true);
+  // list of all librarians stored here
+  const [librarians, setLibrarians] = useState([]);
+
+  const [error, setError] = useState('');
+  const [response, setResponse] = useState('');
+
+  // Uses http get request on backend, only for librarians
+  const getLibrarians = () => {
+    if (!DefaultTheme.currentUser.isPatron) {
+      AXIOS.get('/api/librarians/all/?currentUserId=admin')
+        .then(res => {
+          setLibrarians(res.data);
+          setLoading(false);
+          console.log(res.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  };
+  // get list of librarians once the page is loaded
+  useEffect(() => {
+    getLibrarians();
+  }, []);
   return (
-    <ScrollView>
-      <Text>View all librarians page</Text>
-    </ScrollView>
+    <>
+    <Button 
+      title="View All Shifts"
+      onPress={() => this.props.navigate('ViewAllShifts')}> 
+      </Button>
+    
+      {/* displays the list of librarians */}
+      <FlatList
+        data={librarians}
+        refreshing={loading}
+        onRefresh={() => {
+          setLoading(true);
+          getLibrarians(setLoading, setLibrarians);
+        }}
+        renderItem={({item}) => {
+          console.log(item);
+          return (
+  
+            <LibrarianCard
+              librarian={item}
+
+              someShiftsButton={
+                // button for viewing this librarian's shifts
+                <Button
+                  onPress={() => {
+                    AXIOS.post('/api/librarians/view_librarian_shifts/?currentUserId=admin&librarianId=' + item.idNum)
+                      .then(res => {
+                        setResponse('Their shifts: ' + res.data);
+                        setError('');
+                        console.log(res.data);
+                      })
+                      .catch(e => {
+                        setResponse('');
+                        if (e.response.data.error) {
+                          setError(e.response.data.error);
+                        } else {
+                          setError(e.response.data);
+                        }
+                      });
+                  }}>
+                  View Their Shifts
+                </Button>
+              }
+            />
+          );
+        }}
+      />
+      {/*Dialog box shows errors or the librarian's shifts */}
+      <Portal>
+        <Dialog visible={error || response}>
+          <Dialog.Title>{error ? 'Error' : 'Response'}</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>{error ? error : response}</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() => {
+                setError('');
+                setResponse('');
+              }}>
+              Ok
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
   );
 };
+
 
 export default ViewAllLibrarians;
