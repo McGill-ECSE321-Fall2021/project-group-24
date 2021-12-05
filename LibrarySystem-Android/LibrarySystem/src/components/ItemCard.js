619250fe36105ae7e665d6610365004e08570448
Saagar <saagarkarya@gmail.com>
2021-12-05 16:24:01 -0500

    Refactor ItemCard
    Co-Authored-By: Kevin <48372730+namdgk@users.noreply.github.com>

--- LibrarySystem-Android/LibrarySystem/src/components/ItemCard.js
+++ LibrarySystem-Android/LibrarySystem/src/components/ItemCard.js
@@ -12,218 +12,112 @@ import {
 //any buttons that we want to render.
 const ItemCard = ({item, reservationDetails, buttons}) => {
   const [loading, setLoading] = useState(true);
-  if (
-    reservationDetails != null ||
-    reservationDetails != undefined ||
-    reservationDetails != ''
-  ) {
-    return (
-      <Card
-        style={{
-          marginHorizontal: '5%',
-          borderRadius: 15,
-          elevation: 2,
-          marginVertical: 5,
-        }}>
-        <Card.Content>
-          <Title style={{alignSelf: 'center'}}>{item.itemTitle}</Title>
-          <View
+
+  return (
+    <Card
+      style={{
+        marginHorizontal: '5%',
+        borderRadius: 15,
+        elevation: 2,
+        marginVertical: 5,
+      }}>
+      <Card.Content>
+        <Title style={{alignSelf: 'center'}}>{item.itemTitle}</Title>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
+          <Image
             style={{
-              flexDirection: 'row',
-            }}>
-            <Image
+              marginRight: 20,
+              flex: 1,
+              resizeMode: 'contain',
+            }}
+            loadingIndicatorSource={<Text>loading</Text>}
+            onLoadEnd={() => {
+              setLoading(false);
+              console.log('loaded');
+            }}
+            source={{
+              uri: item.imageUrl,
+            }}
+          />
+          {loading && (
+            <View
               style={{
-                marginRight: 20,
-                flex: 1,
-                resizeMode: 'contain',
-              }}
-              loadingIndicatorSource={<Text>loading</Text>}
-              onLoadEnd={() => {
-                setLoading(false);
-                console.log('loaded');
-              }}
-              source={{
-                uri: item.imageUrl,
-              }}
-            />
-            {loading && (
-              <View
-                style={{
-                  width: '32%',
-                  position: 'absolute',
-                  alignSelf: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Text style={{textAlign: 'center'}}>No image</Text>
-              </View>
-            )}
-            {/*
+                width: '32%',
+                position: 'absolute',
+                alignSelf: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text style={{textAlign: 'center'}}>No image</Text>
+            </View>
+          )}
+          {/*
             Flex allows us to have this View take up 2/{total flex} amount of space.
             This view has a flex of 2, and the image has a flex of 1, so the image will
             take up 1/3 of the space, and the view below with the info will take up the
             rest of the 2/3.
             */}
-            <View style={{flex: 2}}>
-              <Divider />
-
-              <Paragraph>Description: {item.description}</Paragraph>
-              <Divider />
-
-              <Paragraph>Type: {item.type}</Paragraph>
-              {item.author ? (
-                <Paragraph>Author: {item.author}</Paragraph>
-              ) : null}
-              {item.artist ? (
-                <Paragraph>Artist: {item.artist}</Paragraph>
-              ) : null}
-              {item.publisher ? (
-                <Paragraph>Publisher: {item.publisher}</Paragraph>
-              ) : null}
-              {item.recordingLabel ? (
-                <Paragraph>Recording Label: {item.recordingLabel}</Paragraph>
-              ) : null}
-              {item.productionCompany ? (
-                <Paragraph>
-                  Production Company: {item.productionCompany}
-                </Paragraph>
-              ) : null}
-              {item.movieCase ? (
-                <Paragraph>Movie Cast: {item.movieCase}</Paragraph>
-              ) : null}
-              {item.director ? (
-                <Paragraph>Director: {item.director}</Paragraph>
-              ) : null}
-              {item.issueNumber ? (
-                <Paragraph>Issue Number: {item.issueNumber}</Paragraph>
-              ) : null}
-              {item.producer ? (
-                <Paragraph>Producer: {item.producer}</Paragraph>
-              ) : null}
-              {item.genre ? <Paragraph>Genre: {item.genre}</Paragraph> : null}
-              {item.publishDate ? (
-                <Paragraph>Publish Date: {item.publishDate}</Paragraph>
-              ) : null}
-              {item.isReservable ? (
+          <View style={{flex: 2}}>
+            <Paragraph>Type: {item.type}</Paragraph>
+            {item.author ? <Paragraph>Author: {item.author}</Paragraph> : null}
+            {item.artist ? <Paragraph>Artist: {item.artist}</Paragraph> : null}
+            {item.publisher ? (
+              <Paragraph>Publisher: {item.publisher}</Paragraph>
+            ) : null}
+            {item.recordingLabel ? (
+              <Paragraph>Recording Label: {item.recordingLabel}</Paragraph>
+            ) : null}
+            {item.productionCompany ? (
+              <Paragraph>
+                Production Company: {item.productionCompany}
+              </Paragraph>
+            ) : null}
+            {item.movieCase ? (
+              <Paragraph>Movie Cast: {item.movieCase}</Paragraph>
+            ) : null}
+            {item.director ? (
+              <Paragraph>Director: {item.director}</Paragraph>
+            ) : null}
+            {item.issueNumber ? (
+              <Paragraph>Issue Number: {item.issueNumber}</Paragraph>
+            ) : null}
+            {item.producer ? (
+              <Paragraph>Producer: {item.producer}</Paragraph>
+            ) : null}
+            {item.genre ? <Paragraph>Genre: {item.genre}</Paragraph> : null}
+            {item.publishDate ? (
+              <Paragraph>Publish Date: {item.publishDate}</Paragraph>
+            ) : null}
+            {item.isReservable && !reservationDetails ? (
+              <Paragraph>
+                Next Available Date: {item.nextAvailableDate}
+              </Paragraph>
+            ) : null}
+            {item.isReservable && reservationDetails ? (
+              <>
                 <Paragraph>
                   Reservation Start Date: {reservationDetails.startDate}
                 </Paragraph>
-              ) : null}
-              {item.isReservable ? (
                 <Paragraph>
                   Reservation: {reservationDetails.itemReservationId}
                 </Paragraph>
-              ) : null}
-              {item.isReservable ? (
                 <Paragraph>
                   Checked out?: {reservationDetails.isCheckedOut.toString()}
                 </Paragraph>
-              ) : null}
-              {item.isReservable ? (
                 <Paragraph>
                   Num of renewals left: {reservationDetails.numOfRenewalsLeft}
                 </Paragraph>
-              ) : null}
-              <Paragraph>Reserved for: {reservationDetails.idNum}</Paragraph>
-            </View>
+                <Paragraph>Reserved for: {reservationDetails.idNum}</Paragraph>
+              </>
+            ) : null}
           </View>
-        </Card.Content>
-        <Card.Actions style={{alignSelf: 'center'}}>{buttons}</Card.Actions>
-      </Card>
-    );
-  } else {
-    return (
-      <Card
-        style={{
-          marginHorizontal: '5%',
-          borderRadius: 15,
-          elevation: 2,
-          marginVertical: 5,
-        }}>
-        <Card.Content>
-          <Title style={{alignSelf: 'center'}}>{item.itemTitle}</Title>
-          <View
-            style={{
-              flexDirection: 'row',
-            }}>
-            <Image
-              style={{
-                marginRight: 20,
-                flex: 1,
-                resizeMode: 'contain',
-              }}
-              loadingIndicatorSource={<Text>loading</Text>}
-              onLoadEnd={() => {
-                setLoading(false);
-                console.log('loaded');
-              }}
-              source={{
-                uri: item.imageUrl,
-              }}
-            />
-            {loading && (
-              <View
-                style={{
-                  width: '32%',
-                  position: 'absolute',
-                  alignSelf: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Text style={{textAlign: 'center'}}>No image</Text>
-              </View>
-            )}
-            {/*
-            Flex allows us to have this View take up 2/{total flex} amount of space.
-            This view has a flex of 2, and the image has a flex of 1, so the image will
-            take up 1/3 of the space, and the view below with the info will take up the
-            rest of the 2/3.
-            */}
-            <View style={{flex: 2}}>
-              <Paragraph>Type: {item.type}</Paragraph>
-              {item.author ? (
-                <Paragraph>Author: {item.author}</Paragraph>
-              ) : null}
-              {item.artist ? (
-                <Paragraph>Artist: {item.artist}</Paragraph>
-              ) : null}
-              {item.publisher ? (
-                <Paragraph>Publisher: {item.publisher}</Paragraph>
-              ) : null}
-              {item.recordingLabel ? (
-                <Paragraph>Recording Label: {item.recordingLabel}</Paragraph>
-              ) : null}
-              {item.productionCompany ? (
-                <Paragraph>
-                  Production Company: {item.productionCompany}
-                </Paragraph>
-              ) : null}
-              {item.movieCase ? (
-                <Paragraph>Movie Cast: {item.movieCase}</Paragraph>
-              ) : null}
-              {item.director ? (
-                <Paragraph>Director: {item.director}</Paragraph>
-              ) : null}
-              {item.issueNumber ? (
-                <Paragraph>Issue Number: {item.issueNumber}</Paragraph>
-              ) : null}
-              {item.producer ? (
-                <Paragraph>Producer: {item.producer}</Paragraph>
-              ) : null}
-              {item.genre ? <Paragraph>Genre: {item.genre}</Paragraph> : null}
-              {item.publishDate ? (
-                <Paragraph>Publish Date: {item.publishDate}</Paragraph>
-              ) : null}
-              {item.isReservable ? (
-                <Paragraph>
-                  Next Available Date: {item.nextAvailableDate}
-                </Paragraph>
-              ) : null}
-            </View>
-          </View>
-        </Card.Content>
-        <Card.Actions style={{alignSelf: 'center'}}>{buttons}</Card.Actions>
-      </Card>
-    );
-  }
+        </View>
+      </Card.Content>
+      <Card.Actions style={{alignSelf: 'center'}}>{buttons}</Card.Actions>
+    </Card>
+  );
 };
 
 export default ItemCard;
